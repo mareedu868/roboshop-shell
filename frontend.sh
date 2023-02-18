6da9c289c1e48d0917f430b5a9c06d6e36e64428
srini <mareedu868@gmail.com>
2023-02-18 16:09:11 +0530

    frontend code update

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,35 @@
-yum install nginx -y
+code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -rf ${log_file}
+
+print_head() {
+  echo -e "\e[36m$1\e[0m"
+}
+
+print_head "Installing nginx"
+
+yum install nginx -y &>>${log_file}
+
+print_head "Removing Old content"
+
 rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+print_head "Downloading Frontend zip file"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+
+print_head "Extracting Frontend code"
+
+unzip /tmp/frontend.zip &>>${log_file}
+
+print_head "Copying Roboshop Configuration"
+
 cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-systemctl enable nginx
+
+print_head "Enabling and Restarting nginx"
+
+systemctl enable nginx &>>${log_file}
+
 systemctl restart nginx
