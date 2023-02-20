abc0697531edb1fa071c32e2c00fdf88eb3a53b2
srini <mareedu868@gmail.com>
2023-02-20 10:32:21 +0530

    mongodb update

--- common.sh
+++ common.sh
@@ -2,8 +2,7 @@ code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 err_file=/tmp/roboerr.log
 rm -rf ${log_file}
-rm -rf ${err_file}
 
 print_head() {
-  echo -e "\e[35m$1\e[0m"
+  echo -e "\e[34m$1\e[0m"
 }
--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,13 @@
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+source common.sh
+print_head "Creating mongodb repo file "
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+print_head "Installing mongodb"
 yum install mongodb-org -y
+print_head "Enable mongodb service"
 systemctl enable mongod
+print_head "Starting mongodb service"
 systemctl start mongod
-# replace 127.0.0.1 with 0.0.0.0 in /etc/mongod.conf
+print_head "Replace 127.0.0.1 with 0.0.0.0 in /etc/mongod.conf"
+sed -i "s/127.0.0.1/0.0.0.0/g"  /etc/mongod.conf
+print_head "Restarting mongodb service"
 systemctl restart mongod
