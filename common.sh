d57548bb632a8af895d620f4f3aa89f41ff875fd
srini <mareedu868@gmail.com>
2023-02-20 09:29:20 +0530

    catalogue update

--- common.sh
+++ common.sh
@@ -2,7 +2,8 @@ code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 err_file=/tmp/roboerr.log
 rm -rf ${log_file}
+rm -rf ${err_file}
 
 print_head() {
-  echo -e "\e[36m$1\e[0m"
+  echo -e "\e[35m$1\e[0m"
 }
