71ec12e339408e2f1d20135848b1b46fd54d418e
srini <mareedu868@gmail.com>
2023-02-23 14:33:47 +0530

    Catalogue file update

--- catalogue.sh
+++ catalogue.sh
@@ -1,29 +1,4 @@
 source common.sh
-print_head "Downloading nodejs rpm"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-print_head "Installing nodejs"
-yum install nodejs -y &>>${log_file}
-useradd roboshop
-mkdir /app
-rm -rf /app/*
-print_head "Downloading catalogue code"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
-cd /app
-print_head "Extracting catalogue code"
-unzip /tmp/catalogue.zip &>>${log_file}
-print_head "Installing catalogue code"
-npm install &>>${log_file}
-print_head "Creating catalogue service"
-cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
-print_head "Reloading daemon service process"
-systemctl daemon-reload &>>${log_file}
-print_head "Enabling catalogue service"
-systemctl enable catalogue &>>${log_file}
-print_head "Restarting catalogue service"
-systemctl restart catalogue &>>${log_file}
-print_head "Creating mongodb repo"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-print_head "Installing mongodb client"
-yum install mongodb-org-shell -y &>>${log_file}
-print_head "loading catalogue schema"
-mongo --host 172.31.56.202 </app/schema/catalogue.js &>>${log_file}
+component=catalogue
+nodejs
+schema_load
--- common.sh
+++ common.sh
@@ -16,3 +16,76 @@ status_check() {
     exit 1
   fi
 }
+
+nodejs() {
+  print_head "Downloading nodejs rpm"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  status_check $?
+
+  print_head "Installing nodejs"
+  yum install nodejs -y &>>${log_file}
+  status_check $?
+
+  print_head "Creating roboshop user"
+  id roboshop &>>${log_file}
+  if [ $? -ne 0 ]; then
+     useradd roboshop
+  fi
+  status_check $?
+
+  print_head "Creating app directory"
+  if [ ! -d /app ]; then
+   mkdir /app
+  fi
+  status_check $?
+
+  print_head "Removing old content in app directory"
+  rm -rf /app/* &>>{log_file}
+  status_check $?
+
+  print_head "Change directory to app"
+  cd /app
+  status_check $?
+
+  print_head "Downloading ${component} code"
+  curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+  status_check $?
+
+  print_head "Extracting ${component} code"
+  unzip /tmp/${component}.zip &>>${log_file}
+  status_check $?
+
+  print_head "Installing ${component} code"
+  npm install &>>${log_file}
+  status_check $?
+
+  print_head "Creating ${component} service"
+  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  status_check $?
+
+  print_head "Reloading daemon service process"
+  systemctl daemon-reload &>>${log_file}
+  status_check $?
+
+  print_head "Enabling ${component} service"
+  systemctl enable ${component} &>>${log_file}
+  status_check $?
+
+  print_head "Starting ${component} service"
+  systemctl start ${component} &>>${log_file}
+  status_check $?
+}
+
+schema_load() {
+    print_head "Creating mongodb repo"
+    cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+    status_check $?
+
+    print_head "Installing mongodb client"
+    yum install mongodb-org-shell -y &>>${log_file}
+    status_check $?
+
+    print_head "loading ${component} schema"
+    mongo --host mongodb.mydevopslearning.online </app/schema/${component}.js &>>${log_file}
+    status_check $?
+}
