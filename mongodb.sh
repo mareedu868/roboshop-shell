7439b67f647bf6eece53a88921b900b06fd50cdc
srini <mareedu868@gmail.com>
2023-02-23 14:39:38 +0530

    mongodb file update

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,24 @@
 source common.sh
 print_head "Creating mongodb repo file "
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
+
 print_head "Installing mongodb"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log_file}
+status_check $?
+
 print_head "Enable mongodb service"
-systemctl enable mongod
+systemctl enable mongod &>>${log_file}
+status_check $?
+
 print_head "Starting mongodb service"
-systemctl start mongod
+systemctl start mongod &>>${log_file}
+status_check $?
+
 print_head "Replace 127.0.0.1 with 0.0.0.0 in /etc/mongod.conf"
-sed -i "s/127.0.0.1/0.0.0.0/g"  /etc/mongod.conf
+sed -i "s/127.0.0.1/0.0.0.0/g"  /etc/mongod.conf &>>${log_file}
+status_check $?
+
 print_head "Restarting mongodb service"
-systemctl restart mongod
+systemctl restart mongod &>>${log_file}
+status_check $?
